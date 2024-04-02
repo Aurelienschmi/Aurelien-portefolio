afb3643213b0f633847a6ebac91d85ba7ca34448
Aurélien SCHMIEDER <schmieder.aurelien@gmail.com>
2024-04-02 19:46:08 +0200

    update Header to be responsive

--- app/header.tsx
+++ app/header.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex-row justify-ceter justify-center flex-wrap flex items-center ">
+    <header className="flex-row justify-ceter justify-center flex-wrap flex items-center sm: flex-col ">
       <a
         href="/"
-        className="p-2 m-8 border-b-2 border-transparent hover:border-white"
+        className="p-2 m-8 border-b-2 border-transparent hover:border-white sm: m-2 p-1 "
       >
         À propos de moi
       </a>
       <a
         href="/mySkills"
-        className="p-2 m-8 border-b-2 border-transparent hover:border-white"
+        className="p-2 m-8 border-b-2 border-transparent hover:border-white sm: m-2 p-1"
       >
         Mes compétences
       </a>
       <a
         href="/contactMe"
-        className="p-2 m-8 border-b-2 border-transparent hover:border-white"
+        className="p-2 m-8 border-b-2 border-transparent hover:border-white sm: p-1 m-2"
       >
         Contactez moi
       </a>
