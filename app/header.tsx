fcdd449b2a4a48a753f5c7391ffd634566db6a7b
Aurélien SCHMIEDER <schmieder.aurelien@gmail.com>
2024-04-02 20:02:39 +0200

    bug fixe disable sm function

--- app/header.tsx
+++ app/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex-row justify-ceter justify-center flex-wrap flex items-center sm: flex-col ">
+    <header className="flex-row justify-ceter justify-center flex-wrap flex items-center ">
       <a
         href="/"
         className="p-2 m-8 border-b-2 border-transparent hover:border-white sm: m-2 p-1 "
