cbd5478c72ac5216c5be3c0e49dde09227ecc0c5
Aurélien SCHMIEDER <schmieder.aurelien@gmail.com>
2024-04-04 22:40:23 +0200

    responsivity mySkills

--- app/mySkills/page.tsx
+++ app/mySkills/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <div className="bg-black bg-opacity-90 text-white ">
         <Header />
       </div>
-      <main className="flex-col justify-center font-medium text-white h-screen ">
-        <div className="relative bg-black h-4/6">
+      <main className="sm:flex-col justify-center font-medium text-white h-auto">
+        <div className="relative bg-black sm:h-4/6">
           <div
             className="absolute inset-0 bg-fixed bg-no-repeat bg-cover border-b-4 border-t-4 border-white "
             style={{
@@ -23,11 +23,11 @@ export default function Home() {
           />
           <div className="relative flex flex-col items-center py-10 text-white">
             <h2 className="p-4 border-b-2 border-white">Systèmes et réseaux</h2>
-            <h3 className="p-10 pb-0 text-3xl w-1/3">
+            <h3 className="p-10 pb-0 text-3xl sm:w-1/3 w-auto">
               Expertise en Réseautique : Certifications CCNAV007, CCNA et CSNA
               en Cours
             </h3>
-            <p className="text-center break-auto p-10 w-1/3">
+            <p className="text-center break-auto sm:w-1/3 w-auto">
               Certifié CCNAv7 et en cours de préparation pour les certifications
               CCNA et CSNA, je possède une solide expertise en réseautique. Ma
               formation m&apos;a permis de maîtriser les principes fondamentaux
@@ -46,11 +46,11 @@ export default function Home() {
           />
           <div className="relative flex flex-col items-center py-10 text-white">
             <h2 className="p-4 border-b-2 border-white">Développement</h2>
-            <h3 className="p-10 pb-0 text-3xl w-1/3">
+            <h3 className="p-10 pb-0 text-3xl sm:w-1/3 w-auto">
               Polyvalence en Développement : Maîtrise des Langages C, Java,
               Python, HTML, CSS et JavaScript
             </h3>
-            <p className="text-center break-auto p-10 w-1/3">
+            <p className="text-center break-auto sm:w-1/3 w-auto">
               Avec une expérience diversifiée dans les langages de programmation
               tels que le C, Java, Python, HTML, CSS et JavaScript, je possède
               une polyvalence solide en développement logiciel. Ayant travaillé
@@ -71,11 +71,11 @@ export default function Home() {
           />
           <div className="relative flex flex-col items-center py-10 text-white">
             <h2 className="p-4 border-b-2 border-white">Systèmes et réseaux</h2>
-            <h3 className="p-10 pb-0 text-3xl w-1/3">
+            <h3 className="p-10 pb-0 text-3xl sm:w-1/3 w-auto">
               Maîtrise de la Modélisation de Bases de Données et de SQL :
               Conception et Gestion de Données Efficace
             </h3>
-            <p className="text-center break-autos p-10 w-1/3">
+            <p className="text-center break-autos p-10 sm:w-1/3 w-auto">
               Je suis compétent dans la création, la gestion et
               l&apos;utilisation de bases de données, ainsi que dans
               l&apos;écriture de requêtes SQL. Ma capacité à concevoir des
