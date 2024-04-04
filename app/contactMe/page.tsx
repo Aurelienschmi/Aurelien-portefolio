51ad5015fabbf0900a8c0c669743a21081abd35a
Aurélien SCHMIEDER <schmieder.aurelien@gmail.com>
2024-04-04 22:30:17 +0200

    responsivity section contactMe

--- app/contactMe/page.tsx
+++ app/contactMe/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <main className="flex p-20 items-center justify-center items-baseline bg-gradient-to-r from-black to-gray-500 font-medium text-white h-screen ">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSfPxjUFOO0kcR-gOetzN9KUb1oAebuYi7yTOj2UH27obiHNSQ/viewform?embedded=true"
-          width="640"
-          height="640"
+          width="540"
+          height="540"
         >
           Chargement…
         </iframe>
